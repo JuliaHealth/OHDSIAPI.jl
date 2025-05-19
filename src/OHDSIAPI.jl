3fec532e6c2cd63521fabdf3fab087b40c57633b
TheCedarPrince <jacobszelko@gmail.com>
2025-05-18 20:33:03 -0400

    Added downloaders

--- src/OHDSIAPI.jl
+++ src/OHDSIAPI.jl
@@ -2,9 +2,12 @@ module OHDSIAPI
 
 using HTTP 
 using JSON3 
+using ProgressMeter
+using ZipFile
 
 BASE_URL = "https://atlas-demo.ohdsi.org/WebAPI"
 
 include("get.jl")
+include("downloaders.jl")
 
 end
